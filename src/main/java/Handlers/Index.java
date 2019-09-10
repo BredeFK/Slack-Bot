30754225df413c57caa908df4c05b79794cbcbbd
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-10 15:58:07 +0200

    moved slash logic from message to slashcommands

--- src/main/java/Handlers/Index.java
+++ src/main/java/Handlers/Index.java
@@ -6,20 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-// Handlers.Event handles events from slack (https://api.slack.com/events-api)
-@WebServlet("/event")
-public class Event extends HttpServlet {
-    private static final Logger logger = Logger.getLogger(Event.class.getName());
+@WebServlet("/")
+public class Index extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(Index.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        logger.log(Level.INFO, "GET request on /");
+        resp.getWriter().write("<h1>Welcome to my index page</h1><h2>This is all</h2>");
+        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
     }
 }
--- src/main/java/Handlers/Interactive.java
+++ src/main/java/Handlers/Interactive.java
@@ -6,10 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 // Handlers.Interactive handles interactivity from slack users (https://api.slack.com/messaging/interactivity#components)
 @WebServlet("/interactive")
 public class Interactive extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(Interactive.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
