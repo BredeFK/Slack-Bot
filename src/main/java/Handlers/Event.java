8d33332c5392837964048ee6a2c7d13e263e68e5
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-10 16:22:02 +0200

    /event can pass slack test now

--- src/main/java/Handlers/Event.java
+++ src/main/java/Handlers/Event.java
@@ -1,11 +1,16 @@
 package Handlers;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 // Handlers.Event handles events from slack (https://api.slack.com/events-api)
@@ -14,12 +19,34 @@ public class Event extends HttpServlet {
     private static final Logger logger = Logger.getLogger(Event.class.getName());
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        logger.log(Level.INFO, "POST request on /event");
+
+        if (!req.getHeader("X-Slack-Signature").isEmpty()) {
+            StringBuilder jb = new StringBuilder();
+            String line = null;
+            try {
+                BufferedReader reader = req.getReader();
+                while ((line = reader.readLine()) != null)
+                    jb.append(line);
+            } catch (Exception e) { /*report an error*/ }
+
+            try {
+                JSONObject jsonObject = new JSONObject(jb.toString());
+
+                if (!jsonObject.isNull("challenge")) {
+                    resp.setContentType("text/plain;charset=utf-8");
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().write(jsonObject.getString("challenge"));
+                    return;
+                }
+            } catch (JSONException e) {
+                // crash and burn
+                logger.log(Level.WARNING, e.getMessage());
+                throw new IOException("Error parsing JSON request string");
+            }
+        }
+
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
     }
 }
