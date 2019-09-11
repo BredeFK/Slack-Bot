a2209001f2668044284cbadf643b734720d7f3fa
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-11 16:36:43 +0200

    Added class for general functions

--- src/main/java/Classes/GeneralFunctions.java
+++ src/main/java/Classes/GeneralFunctions.java
@@ -1,4 +1,26 @@
 package Classes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class GeneralFunctions {
+
+    public GeneralFunctions() {
+
+    }
+
+    public String getBody(HttpServletRequest req) throws IOException {
+
+        StringBuilder jb = new StringBuilder();
+        String line = null;
+        BufferedReader reader = req.getReader();
+
+
+        while ((line = reader.readLine()) != null) {
+            jb.append(line);
+        }
+
+        return jb.toString();
+    }
 }
