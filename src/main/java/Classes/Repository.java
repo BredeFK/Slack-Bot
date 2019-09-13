7ab2c4ee156e108e7668a420d9daf3576e690979
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-13 13:15:37 +0200

    Change template and finish toJson function

--- src/main/java/Classes/Repository.java
+++ src/main/java/Classes/Repository.java
@@ -1,6 +1,9 @@
 package Classes;
 
+import java.util.Date;
+
 public class Repository {
+    private String channelID;
     private int id;
     private String name;
     private String full_name;
@@ -9,12 +12,16 @@ public class Repository {
     private String description;
     private String language;
     private Boolean fork;
-    private String updated_at;
+    private Date updated_at;
     private License license;
     private Repository source;
 
     public int getId() {
         return id;
+    }
+
+    public void setChannelID(String channelID) {
+        this.channelID = channelID;
     }
 
     public String getName() {
@@ -27,6 +34,10 @@ public class Repository {
 
     public Boolean getFork() {
         return fork;
+    }
+
+    public String getHtml_url() {
+        return html_url;
     }
 
     public String getUrl() {
@@ -55,26 +66,45 @@ public class Repository {
 
     @Override
     public String toString() {
+
+        // Check for and dodge nullpointers
         String _source = (fork) ? source.toString() : "null";
         String _licence = (license != null) ? license.toString() : "null";
         String _language = (language != null) ? language : "N/A";
         String desc = (description != null) ? description : "";
+
         return String.format("%nid: %d%nname: %s%nfull_name: %s%nurl: %s%nhtml_url: %s%ndescription: %s%nlanguage: %s%nfork: %s%nupdated_at: %s%nlicence: {%n %s %n}%nsource: {%n %s %n}%n%n",
-                id,
-                name,
-                full_name,
-                url,
-                html_url,
-                desc,
-                _language,
-                fork,
-                updated_at,
-                _licence,
-                _source);
+                id, name, full_name, url, html_url, desc, _language, fork, updated_at, _licence, _source);
     }
 
     // Returns a string in json format to be sent as a message in slack
     public String toJson() {
-        return "";
+
+        StringBuilder text = new StringBuilder();
+
+        text.append(String.format("`GitHub Repository %s`\\n*%s*\\n", full_name, name));
+
+        if (fork) {
+            text.append(String.format("_Forked from %s _\\n", source.getHtml_url()));
+        }
+
+        if (description != null) {
+            text.append(String.format("```%s```\\n", description));
+        }
+
+        if (language != null) {
+            text.append(String.format(":small_blue_diamond:%s\\t", language));
+        }
+
+        if (license != null) {
+            text.append(String.format(":scales: %s\\t", license.getName()));
+        }
+
+        text.append(String.format("_Last updated on_ %ta %tb %td %tZ %tY", updated_at, updated_at, updated_at, updated_at, updated_at));
+
+        return String.format("{\n\"channel\": \"%s\",\n\"attachments\": [\n{\n\"blocks\": [\n{\n\"type\": \"section\",\n" +
+                "\"text\": {\n\"type\": \"mrkdwn\",\n\"text\": \"%s\"\n}\n},\n{\n\"type\": \"context\",\n\"elements\": [\n" +
+                "{\n\"type\": \"mrkdwn\",\n\"text\": \"%s\"\n}\n]\n}\n]\n}\n]\n}", channelID, text.toString(), html_url);
+
     }
 }
