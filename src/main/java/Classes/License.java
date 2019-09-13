670e1094cdd15654e7e42cd6b682c09d8990363b
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-13 11:45:34 +0200

    Add licence to repository

--- src/main/java/Classes/License.java
+++ src/main/java/Classes/License.java
@@ -1,4 +1,15 @@
 package Classes;
 
 public class License {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\nname: %s", name);
+    }
 }
