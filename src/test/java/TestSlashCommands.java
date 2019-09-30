eb9045535d540a4fcb19159bd3fd92f48c0f220a
Brede Fritjof Klausen <bfk@itverket.no>
2019-09-30 14:54:30 +0200

    Add @Ignore to start of TestSlashCommands

--- src/test/java/TestSlashCommands.java
+++ src/test/java/TestSlashCommands.java
@@ -1,14 +1,36 @@
+import alfred.Main;
+import alfred.controllers.SlashCommands;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.testng.annotations.Test;
 
+@Ignore // TODO : Remove this and finish tests
+@RunWith(SpringRunner.class)
+@WebMvcTest(SlashCommands.class)
+@ContextConfiguration(classes = {Main.class})
 public class TestSlashCommands {
 
     @Test
-    public void TestDoPostQuote() {
+    public void TestPOSTQuote() {
 
     }
 
     @Test
-    public void TestDoPostGithub() {
-        
+    public void TestPOSTGithub() {
+
     }
+
+    @Test
+    public void TestPOSTMannen() {
+
+    }
+
+    @Test
+    public void TestPOSTDovre() {
+
+    }
+
 }
