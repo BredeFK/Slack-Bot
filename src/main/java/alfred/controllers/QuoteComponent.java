a925768ecd2ea9ce35742520e92ac9961001a9b4
Brede Fritjof Klausen <bfk@itverket.no>
2019-10-07 10:22:05 +0200

    Function is working, using service as param

--- src/main/java/alfred/controllers/QuoteComponent.java
+++ src/main/java/alfred/controllers/QuoteComponent.java
@@ -1,8 +1,11 @@
 package alfred.controllers;
 
 import alfred.models.general.EnvVars;
+import alfred.services.DailyQuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -13,23 +16,27 @@ import java.util.logging.Logger;
 @Component
 public class QuoteComponent {
 
+    // Logger
     private static final Logger logger = Logger.getLogger(QuoteComponent.class.getName());
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - HH:mm:ss");
+    // Date format
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - HH:mm:ss Z");
 
     // 60 sec, 60min, 24h, to milliseconds
     private static final int day = 60 * 60 * 24 * 1000;
 
+    // Environmental variables
     private EnvVars envVars = new EnvVars();
 
+    @Autowired // Service for DB
+    private DailyQuoteService dailyQuoteService;
 
-    //@Scheduled(fixedRate = 5000)
+
+    @Scheduled(fixedRate = day)
     public void sendQuote() {
 
-        // 500 error, service is null
-        
         // Get and send quote
-        ResponseEntity<String> responseEntity = new SlashCommands().sendQuote(envVars);
+        ResponseEntity<String> responseEntity = new SlashCommands().sendQuote(envVars, dailyQuoteService);
 
         // Check if it was sent successfully
         if (responseEntity.getStatusCode() == HttpStatus.OK)
